dfbf81c2f7fe0e227bb28616ee3acb5ac341bba6
Sai Kiran Bonthu <SaikiranB1@hexaware.com>
2026-07-16 15:01:46 +0530

    Minor changes in backend

--- fastapi-backend/app/database/database.py
+++ fastapi-backend/app/database/database.py
@@ -90,22 +90,11 @@ from sqlalchemy.orm import (DeclarativeBase,sessionmaker)
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv(
-   "DB_PORT",
-   "5432"
-)
-DB_NAME = os.getenv(
-   "DB_NAME"
-)
-DB_USER = os.getenv(
-   "DB_USER"
-)
-DB_PASSWORD = os.getenv(
-   "DB_PASSWORD"
-)
-INSTANCE_CONNECTION_NAME = os.getenv(
-   "INSTANCE_CONNECTION_NAME"
-)
+DB_PORT = os.getenv("DB_PORT","5432")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
 
 required_variables = {
    "DB_NAME": DB_NAME,
